849877bfe1c295962d2e6c732d70ad2f9d43087b
Alex Chavez <chavezzz8909@gmail.com>
2025-11-25 19:44:45 +0000

    Try fixing this error: `Runtime Error: Error: Switched to client renderi

--- src/components/feed/post-card.tsx
+++ src/components/feed/post-card.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
 import type { KevinPost } from "@/lib/types";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
-import { LocationDisplay } from "./location-display";
+import dynamic from "next/dynamic";
+import { Skeleton } from "../ui/skeleton";
+
+const LocationDisplay = dynamic(() => import("./location-display").then(mod => mod.LocationDisplay), {
+  ssr: false,
+  loading: () => <div className="w-full space-y-3">
+    <Skeleton className="h-40 w-full" />
+    <Skeleton className="h-4 w-2/3" />
+    <Skeleton className="h-4 w-1/2" />
+  </div>
+});
+
 
 interface PostCardProps {
   post: KevinPost;
